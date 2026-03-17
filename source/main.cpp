cc1a54ec2e0faa15194d39f26e622b2fd02140d8
2b-zipper <119087427+2b-zipper@users.noreply.github.com>
2026-03-17 23:05:16 +0900

    Fix crash on exit

--- source/main.cpp
+++ source/main.cpp
@@ -63,6 +63,8 @@ int main(int argc, char **argv) {
   }
 
   UI::ScreenManager::getInstance().shutdown();
+  UI::ImageManager::getInstance().shutdown();
+  Discord::DiscordClient::getInstance().shutdown();
   Network::NetworkManager::getInstance().shutdown();
   psExit();
   romfsExit();
